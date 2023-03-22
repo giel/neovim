25e9c34f017def58fc295554a93fc3442689548d
Giel (mbm1x21) <gscharff@gisaff.nl>
2023-03-22 12:03:14 +0100

    nvim-tree improve; cleanup

--- init.lua
+++ init.lua
@@ -6,8 +6,6 @@ require "autocommands"
 require('keymappings')       -- Keymappings for last to allow for mapping of plugins ?
 
 -- Plugins specific inits
--- require('init.cmp')          -- completion plugin
--- require('init.nvim-comment') -- commenting lines
 -- require('init.project')      -- project detection
 -- require('lsp')               -- global lsp install
 
