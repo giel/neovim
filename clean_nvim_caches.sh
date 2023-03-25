49b7dc9044ab858bf8e5b41c9b44c1a0b0bab381
Giel (mbm1x21) <gscharff@gisaff.nl>
2023-03-25 19:00:04 +0100

    fix project plugin; readme; clean caches

--- clean_nvim_caches.sh
+++ clean_nvim_caches.sh
@@ -2,4 +2,5 @@
 # clean_nvim_caches
 
 rm ~/.config/nvim/plugin/packer_compiled.lua
+rmdir ~/.config/nvim/plugin
 rm -rf ~/.local/share/nvim
--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -1,24 +1,24 @@
-
 return {
-"ahmedkhalf/project.nvim" ,
- commit = "685bc8e3890d2feb07ccf919522c97f7d33b94e4"
+  "ahmedkhalf/project.nvim",
+  commit = "1c2e9c93c7c85126c2197f5e770054f53b1926fb",
+  dependencies = "nvim-telescope/telescope.nvim",
 
+  config = function()
+    require("project_nvim").setup({
 
--- project.setup({
+      -- detection_methods = { "lsp", "pattern" },
+      -- NOTE: lsp detection will get annoying with multiple langs in one project
+      detection_methods = { "pattern" },
 
---   -- detection_methods = { "lsp", "pattern" }, -- NOTE: lsp detection will get annoying with multiple langs in one project
---   detection_methods = { "pattern" },
+      -- patterns used to detect root dir, when **"pattern"** is in detection_methods
+      patterns = { ".git", "Makefile", "package.json" },
+    })
 
---   -- patterns used to detect root dir, when **"pattern"** is in detection_methods
---   patterns = { ".git", "Makefile", "package.json" },
--- })
+    local tele_status_ok, telescope = pcall(require, "telescope")
+    if not tele_status_ok then
+      return
+    end
 
--- local tele_status_ok, telescope = pcall(require, "telescope")
--- if not tele_status_ok then
---   return
--- end
-
--- telescope.load_extension('projects')
-
-
+    telescope.load_extension("projects")
+  end,
 }
