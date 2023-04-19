628594b8822217924c9e3da1edd56c7cef45817f
Giel (mbm1x21) <gscharff@gisaff.nl>
2023-04-19 21:46:35 +0200

    code completion extra esc mapping

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,3 +1,6 @@
+-- https://github.com/hrsh7th/nvim-cmp
+-- code completion
+
 local M = {
   "hrsh7th/nvim-cmp",
   enabled = true,
@@ -76,6 +79,10 @@ function M.config()
         i = cmp.mapping.abort(),
         c = cmp.mapping.close(),
       }),
+      ["<ESC>"] = cmp.mapping({
+        i = cmp.mapping.abort(),
+        c = cmp.mapping.close(),
+      }),
       -- Accept currently selected item. If none selected, `select` first item.
       -- Set `select` to `false` to only confirm explicitly selected items.
       ["<CR>"] = cmp.mapping.confirm({ select = true }),
@@ -147,12 +154,3 @@ function M.config()
 end
 
 return M
-
--- cmp plugins
--- { "hrsh7th/cmp-buffer"        , commit = "3022dbc9166796b644a841a02de8dd1cc1d311fa" }, -- buffer completions
---  { "hrsh7th/cmp-cmdline"       , commit = "23c51b2a3c00f6abc4e922dbd7c3b9aca6992063" }, -- cmdline completions
--- { "hrsh7th/cmp-nvim-lsp"      , commit = "59224771f91b86d1de12570b4070fe4ad7cd1eeb" }, -- neovim's built-in language server client.
--- { "hrsh7th/cmp-nvim-lua"      , commit = "d276254e7198ab7d00f117e88e223b4bd8c02d21" }, -- neovim Lua API.
--- { "hrsh7th/cmp-path"          , commit = "91ff86cd9c29299a64f968ebb45846c485725f23" }, -- path completions
---   { "hrsh7th/nvim-cmp"          , commit = "d09b0c396aaa078cdfe78316ec06940971136e85" }, -- The completion plugin
---   { "saadparwaiz1/cmp_luasnip"  , commit = "18095520391186d634a0045dacaa346291096566" }, -- snippet completions
