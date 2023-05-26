22bcaae98035b033a4ac84f09f44e56ff0893e72
Giel (lenx1ext) <gscharff@gisaff.nl>
2023-05-26 10:31:08 +0200

    Updates and cleanup

--- lua/plugins/bufdelete.lua
+++ lua/plugins/bufdelete.lua
@@ -1,7 +1,7 @@
 -- https://github.com/famiu/bufdelete.nvim
 -- bufdelete allows you to do delete buffers (close files) without closing your windows or messing up your layout.
+-- Replacement of vim-bbye
 return {
   "famiu/bufdelete.nvim", -- provide Bdelete function (instead of vim's bdelete)
-  -- disable vim-bbye if this one is enabled
   enabled = true,
 }
--- lua/plugins/vim-bbye.lua
+++
@@ -1,7 +0,0 @@
--- https://github.com/moll/vim-bbye
--- Bbye allows you to do delete buffers (close files) without closing your windows or messing up your layout.
-return {
-  "moll/vim-bbye", -- provide Bdelete function (instead of vim's bdelete)
-  -- disable bufdelete if this one is enabled
-  enabled = false,
-}
