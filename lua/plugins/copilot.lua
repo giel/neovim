880f646c8656859736baffc202411464b8298265
Giel (mbm1x21) <gscharff@gisaff.nl>
2023-06-27 23:03:31 +0200

    copilot and flash plugins

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,7 @@
+-- https://docs.github.com/en/enterprise-cloud@latest/copilot/getting-started-with-github-copilot?tool=vimneovim
+-- Copilot AI in neovim
+
+return {
+  "github/copilot.vim",
+  enabled = true,
+}
