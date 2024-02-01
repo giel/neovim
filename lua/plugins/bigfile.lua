e2b40ac5742a39a382b673cbff245aaa04e794b7
Giel Scharff <Giel.Scharff@NutsServices.local>
2024-02-01 09:29:26 +0100

    bigfile plugin

---
+++ lua/plugins/bigfile.lua
@@ -0,0 +1,5 @@
+-- https://github.com/LunarVim/bigfile.nvim
+-- This plugin automatically disables certain features if the opened file is big. File size and features to disable are configurable.
+return {
+  "LunarVim/bigfile.nvim",
+}
