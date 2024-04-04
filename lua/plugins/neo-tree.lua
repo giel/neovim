c3c5940d7cfb3af44192c78970156c9361bea173
Giel Scharff <Giel.Scharff@NutsServices.local>
2024-04-04 10:54:15 +0200

    always show .github in neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -24,6 +24,32 @@ return {
       filesystem = {
         follow_current_file = { enabled = true }, -- This will find and focus the file in the active buffer every
         hijack_netrw_behaviour = "open_default",
+        filtered_items = {
+          visible = false, -- when true, they will just be displayed differently than normal items
+          hide_dotfiles = true,
+          hide_gitignored = false,
+          hide_hidden = true, -- only works on Windows for hidden files/directories
+          hide_by_name = {
+            ".DS_Store",
+            "thumbs.db",
+            --"node_modules",
+          },
+          hide_by_pattern = {
+            --"*.meta",
+            --"*/src/*/tsconfig.json",
+          },
+          always_show = { -- remains visible even if other settings would normally hide it
+            ".github",
+            --".gitignored",
+          },
+          never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+            --".DS_Store",
+            --"thumbs.db",
+          },
+          never_show_by_pattern = { -- uses glob style patterns
+            --".null-ls_*",
+          },
+        },
       },
       buffers = { follow_current_file = { enabled = true } }, -- This will find and focus the file in the active buffer every
 
