f6d411cfb5c806a4eaee39ea9b178bda5d87d06d
Giel (mbm1x21) <gscharff@gisaff.nl>
2024-04-14 16:56:53 +0200

    obsidian settings for bloghg

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -32,6 +32,35 @@ return {
           local git = os.getenv("GITPATH") .. "/"
           return assert(git .. "bloghg")
         end,
+        overrides = {
+          notes_subdir = "content/article",
+          note_id_func = function(title)
+            -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+            -- In this case a note with the title 'My new note' will be given an ID that looks
+            -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+            local suffix = ""
+            if title ~= nil then
+              -- If title is given, transform it into valid file name.
+              suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+            else
+              -- If title is nil, just add 4 random uppercase letters to the suffix.
+              for _ = 1, 4 do
+                suffix = suffix .. string.char(math.random(65, 90))
+              end
+            end
+            return suffix
+          end,
+          daily_notes = {
+            -- Optional, if you keep daily notes in a separate directory.
+            folder = "content/post/" .. os.date("%Y"),
+            -- Optional, if you want to change the date format for the ID of daily notes.
+            date_format = "%Y%m%d",
+            -- Optional, if you want to change the date format of the default alias of daily notes.
+            alias_format = "%d-%m-%Y",
+            -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+            template = nil,
+          },
+        },
       },
     },
     {
