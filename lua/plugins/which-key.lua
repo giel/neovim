602664e4f692d325dd62db0d19d556538e61a578
Giel (mbm1x21) <gscharff@gisaff.nl>
2024-07-25 23:43:38 +0200

    which-key fix windows settings

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -3,7 +3,7 @@
 
 return {
   "folke/which-key.nvim",
-  -- dependencies = "nvim-web-devicons",
+  dependencies = "nvim-web-devicons",
   event = "VeryLazy",
   config = function()
     vim.o.timeout = true
@@ -14,11 +14,34 @@ return {
       -- refer to the configuration section below
 
       win = {
-        border = "double", -- none, single, double, shadow
-        position = "bottom", -- bottom, top
-        margin = { 0, 0, 0, 0 }, -- extra window margin [top, right, bottom, left]
-        padding = { 0, 0, 0, 0 }, -- extra window padding [top, right, bottom, left]
-        winblend = 0,
+        -- don't allow the popup to overlap with the cursor
+        no_overlap = true,
+        -- width = 1,
+        -- height = { min = 4, max = 25 },
+        -- col = 0,
+        -- row = math.huge,
+        border = "single", -- none, single, double, shadow
+        padding = { 0, 0 }, -- extra window padding [top/bottom, right/left]
+        title = true,
+        title_pos = "left",
+        zindex = 1000,
+        -- Additional vim.wo and vim.bo options
+        bo = {},
+        wo = {
+          -- winblend = 10, -- value between 0-100 0 for fully opaque and 100 for fully transparent
+        },
+      },
+      layout = {
+        width = { min = 20 }, -- min and max width of the columns
+        spacing = 3, -- spacing between columns
+      },
+      keys = {
+        scroll_down = "<c-d>", -- binding to scroll down inside the popup
+        scroll_up = "<c-u>", -- binding to scroll up inside the popup
+      },
+      icons = {
+        separator = "", -- symbol used between a key and it's label
+        group = "+", -- symbol prepended to a group
       },
       presets = {
         operators = true, -- adds help for operators like d, y, ...
