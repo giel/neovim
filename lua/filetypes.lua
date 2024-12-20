66a730e1b0a1253ae8bba5720dc4b30cf3aa6b04
Giel (Giels-MBP) <giel.scharff@budgetthuis.nl>
2024-12-20 05:25:44 +0100

    add terraform comment string

--- lua/filetypes.lua
+++ lua/filetypes.lua
@@ -37,3 +37,11 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   pattern = "*.tmpl",
   callback = set_dynamic_filetype,
 })
+
+-- Set commentstring for HCL files
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "terraform",
+    callback = function()
+        vim.bo.commentstring = "# %s"
+    end,
+})
