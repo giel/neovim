8e936a9fd37a7f70204c3f11dc6cf6f0c5bc9829
Giel <mgsch@mac.com>
2025-11-21 08:57:11 +0100

    neotree config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -46,6 +46,7 @@ return {
             ".gitignore",
             ".obsidian",
             ".octopus",
+            ".zshrc",
           },
           never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
             ".DS_Store",
