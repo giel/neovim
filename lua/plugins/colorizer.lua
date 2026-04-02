1e251c7df992c4d0f924a4a308341833835d5114
Giel (mbm5x26) <mgsch@mac.com>
2026-04-02 21:22:59 +0200

    updates for 0.12: clorizer, treesitter

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,22 +1,97 @@
--- https://github.com/norcalli/nvim-colorizer.lua
+-- https://github.com/catgoose/nvim-colorizer.lua
+-- replaces https://github.com/norcalli/nvim-colorizer.lua
 -- * colors the css hex values, like #FEDC56 or Red
 
 return {
-
-  "norcalli/nvim-colorizer.lua",
+ "catgoose/nvim-colorizer.lua",
   enabled = true,
   event = { "BufReadPre", "BufNewFile" },
-
   config = function()
-    require("colorizer").setup({ "*" }, {
-      RGB = true, -- #RGB hex codes
-      RRGGBB = true, -- #RRGGBB hex codes
-      RRGGBBAA = true, -- #RRGGBBAA hex codes
-      rgb_fn = true, -- CSS rgb() and rgba() functions
-      hsl_fn = true, -- CSS hsl() and hsla() functions
-      css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-      css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-      -- names    = true; -- "Name" codes like Blue
+    require("colorizer").setup({
+      filetypes = { "*" },  -- filetypes to highlight, "*" for all
+      buftypes = {},        -- buftypes to highlight
+      user_commands = true, -- enable user commands (ColorizerToggle, etc.)
+      lazy_load = false,    -- lazily schedule buffer highlighting
+      options = {
+        parsers = {
+          css = false,    -- preset: enables names, hex, rgb, hsl, oklch, css_var
+          css_fn = false, -- preset: enables rgb, hsl, oklch
+          names = {
+            enable = true,          -- enable named colors (e.g. "Blue")
+            lowercase = true,       -- match lowercase names
+            camelcase = true,       -- match CamelCase names (e.g. "LightBlue")
+            uppercase = false,      -- match UPPERCASE names
+            strip_digits = false,   -- ignore names with trailing digits (e.g. "blue3")
+            custom = false,         -- custom name-to-hex mappings; table|function|false
+            extra_word_chars = "-", -- extra chars treated as part of color name
+          },
+          hex = {
+            default = true,        -- default value for unset format keys (see above)
+            rgb = true,            -- #RGB (3-digit)
+            rgba = true,           -- #RGBA (4-digit)
+            rrggbb = true,         -- #RRGGBB (6-digit)
+            rrggbbaa = false,      -- #RRGGBBAA (8-digit)
+            hash_aarrggbb = false, -- #AARRGGBB (QML-style, alpha first)
+            aarrggbb = false,      -- 0xAARRGGBB
+            no_hash = false,       -- hex without '#' at word boundaries
+          },
+          rgb = { enable = false },       -- rgb()/rgba() functions
+          hsl = { enable = false },       -- hsl()/hsla() functions
+          oklch = { enable = false },     -- oklch() function
+          hwb = { enable = false },       -- hwb() function (CSS Color Level 4)
+          lab = { enable = false },       -- lab() function (CIE Lab)
+          lch = { enable = false },       -- lch() function (CIE LCH)
+          css_color = { enable = false }, -- color() function (srgb, display-p3, a98-rgb, etc.)
+          tailwind = {
+            enable = false,                -- parse Tailwind color names
+            update_names = false,          -- feed LSP colors back into name parser (requires both enable + lsp.enable)
+            lsp = {                        -- accepts boolean, true is shortcut for { enable = true, disable_document_color = true }
+              enable = false,                -- use Tailwind LSP documentColor
+              disable_document_color = true, -- auto-disable vim.lsp.document_color on attach
+            },
+          },
+          sass = {
+            enable = false,                    -- parse Sass color variables
+            parsers = { css = true },          -- parsers for resolving variable values
+            variable_pattern = "^%$([%w_-]+)", -- Lua pattern for variable names
+          },
+          xterm = { enable = false },       -- xterm 256-color codes (#xNN, \e[38;5;NNNm)
+          xcolor = { enable = false },      -- LaTeX xcolor expressions (e.g. red!30)
+          hsluv = { enable = false },       -- hsluv()/hsluvu() functions
+          css_var_rgb = { enable = false }, -- CSS vars with R,G,B (e.g.                  -- color: 240,198,198)
+          css_var = {
+            enable = false,           -- resolve var(                          -- name) references to their defined color
+            parsers = { css = true }, -- parsers for resolving variable values
+          },
+          custom = {}, -- list of custom parser definitions
+        },
+        display = {
+          mode = "background", -- string or list: "background"|"foreground"|"underline"|"virtualtext"
+          background = {
+            bright_fg = "#000000", -- text color on bright backgrounds
+            dark_fg = "#ffffff",   -- text color on dark backgrounds
+          },
+          virtualtext = {
+            char = "■",             -- character used for virtualtext
+            position = "eol",       -- "eol"|"before"|"after"
+            hl_mode = "foreground", -- "background"|"foreground"
+          },
+          priority = {
+            default = 150, -- extmark priority for normal highlights
+            lsp = 200,     -- extmark priority for LSP/Tailwind highlights
+          },
+          disable_document_color = true, -- true (all LSPs) | false | { lsp_name = true, ... }
+        },
+        hooks = {
+          should_highlight_line = false,  -- function(line, bufnr, line_num) -> bool
+          should_highlight_color = false, -- function(rgb_hex, parser_name, ctx) -> bool
+          transform_color = false,        -- function(rgb_hex, ctx) -> string
+          on_attach = false,              -- function(bufnr, opts)
+          on_detach = false,              -- function(bufnr)
+        },
+        always_update = false, -- update highlights even in unfocused buffers
+        debounce_ms = 0,       -- debounce highlight updates (ms); 0 = no debounce
+      },
     })
   end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,75 +1,13 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter
 -- syntax higlighting and more
+-- This is a full, incompatible, rewrite: since neovim 0.12
 
-local Plugin = { "nvim-treesitter/nvim-treesitter" }
+return {
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = ':TSUpdate'
+}
 
-Plugin.build = function()
-  require("nvim-treesitter.install").update({ with_sync = true })
-end
 
-Plugin.config = function()
-  local treesitter = require("nvim-treesitter.configs")
-
-  treesitter.setup({
-    ensure_installed = {
-      "bash",
-      "c",
-      "c_sharp",
-      "css",
-      "go",
-      "gomod",
-      "hcl",
-      "html",
-      "json",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "python",
-      "query",
-      "toml",
-      "vim",
-      "vimdoc",
-      "yaml",
-    },
-    sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-    ignore_install = { "" }, -- List of parsers to ignore installing for "all"
-    -- check this together with mason
-    auto_install = true,
-    autopairs = {
-      enable = true,
-    },
-    highlight = {
-      enable = true, -- false will disable the whole extension
-      disable = { "" }, -- list of language that will be disabled
-      additional_vim_regex_highlighting = true,
-    },
-    indent = { enable = true, disable = { "yaml" } },
-    context_commentstring = {
-      enable = true,
-      enable_autocmd = false,
-    },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        -- todo add to keymaps
-        -- set to `false` to disable one of the mappings
-        init_selection = "<CR>",
-        node_incremental = "<CR>",
-        scope_incremental = "<TAB>",
-        node_decremental = "<BS>",
-      },
-    },
-  })
-
-  local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
-  parser_configs.hcl = {
-    filetype = "hcl",
-    "terraform",
-  }
-end
-
-return Plugin
-
--- Use `TSInstallInfo` to see available modules
 -- Use `TSInstall` modulename to actually install modulename
 -- TSInstall bash c_sharp css go gomod hcl html json lua markdown python toml yaml
